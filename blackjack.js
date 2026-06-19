7da63951d5b3cb526f832c9851a9c67eaf65f9f5
Leandro Garibaldi Ferreiro <lgaribaldi86@gmail.com>
2026-06-19 15:45:08 +0200

    Update blackjack.js

--- blackjack.js
+++ blackjack.js
@@ -2,24 +2,27 @@ let deck         = [];
 const tipos      = ['C','D','H','S']; 
 const especiales = ['A','J','Q','K'];
 
-// Exportamos esta función para poder iniciar el juego desde main.js
+// Es obligatorio usar "export" para que main.js pueda usar esta función
 export const crearDeck = () => {
     deck = [];
+    // Generar cartas numéricas
     for( let i = 2; i <= 10; i++ ) {
         for( let tipo of tipos ) {
             deck.push( i + tipo );
         }
     }
+    // Generar cartas de letras (figuras)
     for( let tipo of tipos ) {
         for( let esp of especiales ) {
             deck.push( esp + tipo );
         }
     }
+    // Mezclar el deck de forma aleatoria
     deck = deck.sort(() => Math.random() - 0.5);
     return deck;
 };
 
-// Exportamos la función para pedir carta
+// Es obligatorio usar "export"
 export const pedirCarta = () => {
     if ( deck.length === 0 ) {
         throw 'No hay cartas en el deck';
@@ -27,7 +30,7 @@ export const pedirCarta = () => {
     return deck.pop();
 };
 
-// Exportamos la función para calcular el valor
+// Es obligatorio usar "export"
 export const valorCarta = ( carta ) => {
     const valor = carta.substring(0, carta.length - 1);
     return ( isNaN( valor ) ) ? 
