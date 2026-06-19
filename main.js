0d31dca250badbbab08d3c25d9799edb951f06d8
Leandro Garibaldi Ferreiro <lgaribaldi86@gmail.com>
2026-06-19 15:46:19 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,42 +1,44 @@
-// 1. Estilos y framework
+// Importaciones
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./style.css";
 
-// 2. Funciones 
-import { crearDeck, pedirCarta, valorCarta } from './blackjack';
+// Funciones
+import { crearDeck, pedirCarta, valorCarta } from './blackjack.js';
 
-// 3. Variables de puntuación locales del DOM
 let puntosJugador = 0;
 
-// 4. Referencias de los elementos del HTML
+// Asignaciones index.html
 const btnPedir   = document.querySelector('#btnPedir');
 const btnDetener = document.querySelector('#btnDetener');
 const puntosHTML = document.querySelectorAll('small');
 const divCartasJugador = document.querySelector('#jugador-cartas');
 
-// Inicializamos el deck al cargar la página
+// Inicializamos el deck llamando a la función del módulo importado
 crearDeck();
 
-// 5. Evento del botón Pedir Carta
+// Evento para el botón de pedir carta
 btnPedir.addEventListener('click', () => {
     const carta = pedirCarta();
     
-    // Calcular y actualizar puntos en el HTML
+    // Sumar y renderizar puntos
     puntosJugador = puntosJugador + valorCarta( carta );
     puntosHTML[0].innerText = puntosJugador;
     
-    // Crear el texto de la carta de forma temporal en la pantalla
+    // Crear el elemento visual temporal en el HTML
     const txtCarta = document.createElement('span');
-    txtCarta.innerText = `[${carta}] `;
+    txtCarta.innerText = ` [${carta}] `;
+    txtCarta.style.color = "white";
+    txtCarta.style.fontSize = "20px";
+    txtCarta.style.fontWeight = "bold";
     divCartasJugador.append(txtCarta);
 
-    // Reglas básicas del juego
+    // Validar reglas del Blackjack
     if ( puntosJugador > 21 ) {
         alert('Lo siento, te has pasado de 21. ¡Perdiste!');
         btnPedir.disabled   = true;
         btnDetener.disabled = true;
     } else if ( puntosJugador === 21 ) {
-        alert('¡21 perfectos! Turno de la computadora.');
+        alert('¡21 perfectos! Excelente.');
         btnPedir.disabled   = true;
         btnDetener.disabled = true;
     }
